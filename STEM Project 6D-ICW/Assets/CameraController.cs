85c6c208bb905afcc08cb5a82e4f6a6e96eb54ff
Rube Boex <rube.boex@donboscohalle.be>
2024-09-06 18:48:57 +0200

    commit

--- STEM Project 6D-ICW/Assets/CameraController.cs
+++ STEM Project 6D-ICW/Assets/CameraController.cs
@@ -25,7 +25,7 @@ public class CameraController : MonoBehaviour
         target = povs[index].position;
 
     }
-    private void Fixedupdate()
+    private void FixedUpdate()
     {
         transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
         transform.forward = povs[index].forward;
