8908a91bd26db59c2a89ffe71654cabf0b72d36e
Rube Boex <rube.boex@donboscohalle.be>
2024-09-06 17:41:56 +0200

    cameracontroller

---
+++ STEM Project 6D-ICW/Assets/CameraController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [ToolboxItem("An array of transformers representing camera positions")]
+    [SerializeField] Transform[] povs;
+    [ToolboxItem("The speed at wich the camera follows the plane")]
+    [SerializeField] float speed;
+
+    private int index = 1;
+    private Vector3 target;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) index = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha1)) index = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha1)) index = 2;
+        else if (Input.GetKeyDown(KeyCode.Alpha1)) index = 3;
+
+        target = povs[index].position;
+
+    }
+    private void Fixedupdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.forward = povs[index].forward;
+    }
+}
