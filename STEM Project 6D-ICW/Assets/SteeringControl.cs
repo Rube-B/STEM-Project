62d5215adfd0b43fe8a92452fa7533fe987b85fb
NOODLE <cactus2000@outlook.be>
2024-09-19 00:06:00 +0200

    fix wheel collider
    jah

--- STEM Project 6D-ICW/Assets/SteeringControl.cs
+++ STEM Project 6D-ICW/Assets/SteeringControl.cs
@@ -1,25 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SteeringControl : MonoBehaviour
 {
-
-
-    public WheelCollider frontWheel; // Sleep hier je voorste WheelCollider in vanuit de inspector
+    public WheelCollider frontWheel; // Dit is de variabele die je ziet in de Inspector
     public float maxSteeringAngle = 30f; // Maximale stuurhoek
 
     void Update()
     {
-        // Haal de stuurinvoer op (pijltjestoetsen of een andere input)
         float steerInput = Input.GetAxis("Horizontal");
-
-        // Bereken de stuurhoek
         float steeringAngle = maxSteeringAngle * steerInput;
-
-        // Pas de stuurhoek toe op de voorste WheelCollider
         frontWheel.steerAngle = steeringAngle;
     }
 }
-
-
